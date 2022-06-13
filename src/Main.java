8c67df2871b2fc9501d0a11dd065737a620e326d
Olga Kusakina <kusakina_olya@mail.ru>
2022-06-13 19:11:38 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 4274;
+        int miles = 20;
+        int bonus = price / miles;
+
+        System.out.println("Начислено миль:" + bonus);
+    }
+
+}
