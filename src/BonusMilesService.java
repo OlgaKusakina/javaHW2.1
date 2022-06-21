320f5db861a87f92ba7a5184f5b9ca441d7c1dc5
Olga Kusakina <kusakina_olya@mail.ru>
2022-06-21 16:59:48 +0300

    BonusMilesServes

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,14 +1,13 @@
 public class BonusMilesService {
 
     public int calculate (int cost) {
-
         int bonus;
                 if (cost < 20) {
                     bonus = 0;
                 }
                 else {
                     bonus = cost / 20;
-        }
+                }
         return bonus;
     }
 }
